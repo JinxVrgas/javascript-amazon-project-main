21076f05dde988cafee24f5dfe821186ea5f7995
JinsonVarghese <jinsonvarghese9048@gmail.com>
2024-09-30 13:10:18 +0530

    make add to cart interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -44,7 +44,10 @@ ${product.name}
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button 
+          button-primary 
+          js-add-to-cart
+          "data-product-id ='${product.id}'>
             Add to Cart
           </button>
         </div>`;
@@ -52,5 +55,30 @@ ${product.name}
 });
 
 
-console.log(productsHTML);
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+    .forEach((button) => {
+        button.addEventListener('click', () => {
+            const productId = (button.dataset.productId);
+            let matchingItem;
+            cart.forEach((item) => {
+                if (productId === item.productId) {
+                    matchingItem = item;
+                }
+            });
+            if (matchingItem) {
+                matchingItem.Quantity += 1;
+            } else {
+                cart.push({
+                    productId,
+                    Quantity: 1,
+                });
+            }
+
+
+
+            console.log(cart);
+        });
+
+    });
